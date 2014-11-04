72ff092ed92370007b2dfa1df4f8e334f81dbddc
Thomas Wood <tw1509@doc.ic.ac.uk>
2014-11-04 17:06:27 +0000

    Disallow users not in a group

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -7,7 +7,7 @@ class UserSession
     network = Network.new
     user = network.authenticate(auth_opts)
 
-    if user
+    if user and user_permitted(user)
       user["access_token"] = auth_opts[:access_token]
       return User.new(user)
     else
@@ -17,5 +17,11 @@ class UserSession
     user
   rescue
     nil
+  end
+
+  def user_permitted(user)
+    groups = Network.new.groups({'private_token' => user['private_token']})
+    groups.map! {|g| g['id'] }
+    return !(groups & Settings.users.allow_login_only_from_groups).empty?
   end
 end
